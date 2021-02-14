7b8d8beb23cbb00d97ca4d6113da5722dd24fc73
Pierre-Marie Poitevin <p4e@fb.com>
2021-02-13 23:43:10 -0800

    t

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <h1>We now have Auth!</h1>
       </header>
       <AmplifySignOut />
